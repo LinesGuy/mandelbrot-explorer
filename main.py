2fd51960cabf355e28442e0f76d4149a576e4e9a
LinesGuy <60029482+LinesGuy@users.noreply.github.com>
2021-03-02 19:39:55 +0000

    mouse buttons to zoom

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
-width, height = 64, 64  # RESOLUTION OF FRACTAL BEFORE SCALING
-scale = 8
+width, height = 100, 100  # RESOLUTION OF FRACTAL BEFORE SCALING
+scale = 5
 s_width, s_height = scale*width, scale*height  # SCREEN SIZE
 
 speed = 0.1  # Zoom / movement speed
@@ -18,7 +18,12 @@ running = True
 
 iter_num = 30
 
+frame = 0
+
 while running:
+    frame += 1
+    print(frame)
+
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
@@ -26,6 +31,7 @@ while running:
 
     # Movement/zoom stuffs
     mouse_x, mouse_y  = pygame.mouse.get_pos()
+    mouse_left, _, mouse_right = pygame.mouse.get_pressed(num_buttons=3)
     tx, ty = mouse_x / s_width, mouse_y / s_height
     fx, fy = mx1 + (mx2 - mx1) * tx, my1 + (my2 - my1) * ty
     keys = pygame.key.get_pressed()
@@ -41,13 +47,13 @@ while running:
     if keys[pygame.K_s]:
         iter_num -= 1
 
-    if keys[pygame.K_q]:  # zoom out
+    if keys[pygame.K_q] or mouse_right:  # zoom out
         mx1 = mx1 + (fx - mx1) * -speed
         mx2 = mx2 + (fx - mx2) * -speed
         my1 = my1 + (fy - my1) * -speed
         my2 = my2 + (fy - my2) * -speed
         zoom /= (1 + speed) ** 2
-    if keys[pygame.K_e]:  # zoom in
+    if keys[pygame.K_e] or mouse_left:  # zoom in
         mx1 = mx1 + (fx - mx1) * speed
         mx2 = mx2 + (fx - mx2) * speed
         my1 = my1 + (fy - my1) * speed
@@ -56,6 +62,7 @@ while running:
 
     # The juicy stuff
     img = pygame.Surface((width, height))
+
     for y in range(height):
         for x in range(width):
             sx = (x / width) * (mx2 - mx1) + mx1
